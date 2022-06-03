52ea3410dcef91bb641591d61c5e32d6120e6c73
Dmitry <82148972+dmzur@users.noreply.github.com>
2022-06-03 05:12:41 +0300

    miniWin

--- graph/src/components/BarChart.vue
+++ graph/src/components/BarChart.vue
@@ -1,98 +1,98 @@
 <template>
-    <Bar :chart-options="chartOptions" :chart-data="chartData" :chart-id="chartId" :dataset-id-key="datasetIdKey"
-        :plugins="plugins" :css-classes="cssClasses" :styles="styles" :width="width" :height="height" />
+  <Bar :chart-options="chartOptions" :chart-data="chartData" :chart-id="chartId" :dataset-id-key="datasetIdKey"
+    :plugins="plugins" :css-classes="cssClasses" :styles="styles" :width="width" :height="height" />
 </template>
 
 <script>
 import { Bar } from 'vue-chartjs/legacy'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-import { mapGetters } from 'vuex'
+// import { mapGetters } from 'vuex'
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
-    name: 'BarChart',
-    components: { Bar },
-    props: {
-        chartId: {
-            type: String,
-            default: 'bar-chart'
-        },
-        datasetIdKey: {
-            type: String,
-            default: 'label'
-        },
-        width: {
-            type: Number,
-            default: 400
-        },
-        height: {
-            type: Number,
-            default: 400
-        },
-        cssClasses: {
-            default: '',
-            type: String
-        },
-        styles: {
-            type: Object,
-            default: () => { }
-        },
-        plugins: {
-            type: Object,
-            default: () => { }
-        }
+  name: 'BarChart',
+  components: { Bar },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
     },
-    data() {
-        return {
-            chartData: {
-                labels: ['Food', 'Transport', 'Education', 'Entertainment', 'Sport'],
-                datasets: [{ data: this.getPaymentList }]
-            },
-            chartOptions: {
-                responsive: true
-            }
-        }
+    datasetIdKey: {
+      type: String,
+      default: 'label'
     },
-    computed: {
-        ...mapGetters([
-            'getPaymentsList'
-        ])
+    width: {
+      type: Number,
+      default: 400
     },
-    methods: {
-        // convertDBtoChart() {
-        //     const categories = this.getPaymentsList.map(el => el.category)
-        //     const uniqueCategories = categories.filter((val, idx, self) => self.indexOf(val) === idx)
-        //     this.data = uniqueCategories.map((el) => {
-        //         return {
-        //             x: el,
-        //             y: this.getPaymentsList.filter(item => item.category === el)
-        //                 .reduce((acc, cur) => acc + cur.value, 0),
-        //             r: (100 - Math.ceil(
-        //                 this.getPaymentsList.filter(item => item.category === el)
-        //                     .reduce((acc, cur) => acc + cur.value, 0) *
-        //                 100 /
-        //                 this.getPaymentsList.reduce((acc, cur) => acc + cur.value, 0)
-        //             )) + '%'
-        //         }
-        //     })
-            
-        // }
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => { }
+    },
+    plugins: {
+      type: Object,
+      default: () => { }
+    }
+  },
+  data() {
+    return {
+      chartData: {
+        labels: ['Food', 'Transport', 'Education', 'Entertainment', 'Sport'],
+        datasets: [{
+          data: this.con
+        }]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  },
+  computed: {
+
+    getPL() {
+      return this.$store.getters.getPaymentsList
+    },
+    con() {
+      const test = [];
+      for (let l in this.chartData.labels) {
+        const filtred = this.$store.getters.getPaymentsList.filter(f => f.category === this.chartData.labels[l]);
+        const total = filtred.reduce((acc, f) => acc + f.value, 0);
+        test.push(total)
+        
+      }  
+      return test;  
+      
+    }
+    // takeArr() {
+    //   this.chartData.labels.forEach(l => {
+    //     let test = this.getPL.filter(f => f.category === this.chartData.labels[l]);
+    //     console.log(test);
+    //     const total = test.reduce((acc, f) => acc + f.value, 0);
+    //     console.log(total);
+    //   })
     // },
-    // created() {
-    //     setTimeout(() => {
-    //         this.convertDBtoChart()
-    //     }, 2000)
-    // }
-    // async created() {
-    //     await this.$store.dispatch('fetchData')
-    //     // await console.log(getPaymentsList)
-    // },
-    // mounted() {
-    //     const category = this.getPaymentsList.map(el => el.category)
-    //     console.log(category)
-    // }
-}
+  },
+  // methods: {
+  //   takeArr() {
+  //     for ( l in this.chartData.labels){
+  //       let test = this.getPL.filter(f => f.category === this.chartData.labels[l]);
+  //       console.log(test);
+  //       const total = test.reduce((acc, f) => acc + f.value, 0);
+  //       console.log(total);
+  //       return total
+  //     }
+  //   }
+  // }
+
 }
 </script>
 
